d65da2ff8d2eced85ed070939dc0dd433a3bd3b2
jprominski <jmp3mk@virginia.edu>
2017-09-19 09:29:53 -0400

    KNN update

--- knn(from scratch).R
+++ knn(from scratch).R
@@ -33,15 +33,14 @@ knn.fn(mytrain2,3,sparse_99_col_index,9)
 
 
 
-knn2 <- function(mat, k){
-  n <- nrow(mat)
-  if (n <= k) stop("k can not be more than n-1")
-  neigh <- matrix(0, nrow = n, ncol = k)
+knn2 <- function(sample, k){
+  n <- nrow(sample)
+  neighbor <- matrix(0, nrow = n, ncol = k)
   for(i in 1:n) {
-    euc.dist <- colSums((mat[i, ] - t(mat)) ^ 2)  
-    neigh[i, ] <- order(euc.dist)[2:(k + 1)]
+    euc.dist <- colSums((sample[i, ] - t(sample)) ^ 2)  
+    neighbor[i, ] <- order(euc.dist)[2:(k + 1)]
   }
-  return(neigh)
+  return(neighbor)
 }
 
 mytrain3 <- mytrain2[sparse_99_col_index]
