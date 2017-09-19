5f3f1e6ca332bc80000697ec0aff2c9060e91589
jprominski <jmp3mk@virginia.edu>
2017-09-18 20:45:58 -0400

    KNN update

--- knn(from scratch).R
+++ knn(from scratch).R
@@ -29,6 +29,27 @@ knn.fn<-function(sample,y_col,x_cols,k){
   }
   return(results)
 }
-knn.fn(mytrain,3,sparse_99_col_index,9)
+knn.fn(mytrain2,3,sparse_99_col_index,9)
 
- 
+
+
+knn2 <- function(mat, k){
+  n <- nrow(mat)
+  if (n <= k) stop("k can not be more than n-1")
+  neigh <- matrix(0, nrow = n, ncol = k)
+  for(i in 1:n) {
+    euc.dist <- colSums((mat[i, ] - t(mat)) ^ 2)  
+    neigh[i, ] <- order(euc.dist)[2:(k + 1)]
+  }
+  return(neigh)
+}
+
+mytrain3 <- mytrain2[sparse_99_col_index]
+
+scores <- knn2(mytrain3,3)
+
+scores[333,]
+
+mean(scores)
+
+
