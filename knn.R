576d84251e367f15083b4b4c7d0c2d002b35aab3
jprominski <jmp3mk@virginia.edu>
2017-09-20 15:19:38 -0400

    KNN update w submission

--- knn.R
+++ knn.R
@@ -1,47 +1,62 @@
 library(tidyverse)
 
+
 Mode <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
 
+# VALIDATION
+# train <- comb_clean_final[comb_clean_final$dataset == 'train', ]
+# samp <- sample(1:981, 784, replace = FALSE)
+# mytrain <- train[samp,]
+# myvalid <- train[-samp,]
+# mytrain_knn <- mytrain[,3:ncol(comb_clean_final)]
+# myvalid_knn <- myvalid[,3:ncol(comb_clean_final)]
+
+# VALIDATION
+# knn_j <- function(x){
+#   tr_matrix <- rbind(mytrain_knn,x)
+#   dist_matrix <- as.matrix(dist(tr_matrix))
+#   diag(dist_matrix) <- NA
+#   nearest <- head(order(dist_matrix[,ncol(dist_matrix)]), n=k) %>% mytrain[.,]
+#   score <- Mode(nearest$sentiment)
+#   return(score)
+# }
+
+# VALIDATION
+# k <- 15
+# sentiment <- numeric()
+# sentiment <- apply(myvalid_knn, 1, knn_j)
+# sentiment_knn <- sentiment
+# results <- as.data.frame(cbind(myvalid$sentiment, sentiment_knn))
+# results$correct <- results$V1 == results$sentiment_knn
+# mean(results$correct)
+## Test with k=3, 48.2% accuracy
+## Test with k=1, 45.7% accuracy
+## Test with k=5, 54.3% accuracy
+## Test with k=10, 67.0% accuracy
+## Test with k=15, 69.0% accuracy
+## Test with k=20, 69.0% accuracy
+
+
+train <- comb_clean_final[comb_clean_final$dataset == 'train', 3:ncol(comb_clean_final)]
+test <- comb_clean_final[comb_clean_final$dataset == 'test', 3:ncol(comb_clean_final)]
+
 knn_j <- function(x){
-  tr_matrix <- rbind(comb_clean[comb_clean$dataset == 'train',sparse_99_col_index],x)
+  tr_matrix <- rbind(train,x)
   dist_matrix <- as.matrix(dist(tr_matrix))
   diag(dist_matrix) <- NA
-  nearest <- head(order(dist_matrix[,ncol(dist_matrix)]), n=k) %>% comb_clean[.,]
+  nearest <- head(order(dist_matrix[,ncol(dist_matrix)]), n=k) %>% comb_clean_final[.,]
   score <- Mode(nearest$sentiment)
   return(score)
 }
 
-k <- 3
-score <- numeric()
-test <- comb_clean[comb_clean$dataset == 'test',sparse_99_col_index]
-score <- apply(test, 1, knn_j)
+k <- 15
+sentiment <- numeric()
+sentiment <- apply(test, 1, knn_j)
 
-
-
-# JO KNN
-
-# knn.fn<-function(sample,y_col,x_cols,k){
-#   #y_col: column number of y in sample
-#   #x_cols: list of column numbers of predictors(x) in sample
-#   distance<-function(myrow,compare_row){
-#     return(sqrt(sum((myrow-compare_row)^2)))}
-#   results<-c()
-#   for (i in 1:nrow(sample)){
-#     distance_list<-c()
-#     thesample<-sample[-i,]
-#     for (j in 1:nrow(thesample)){
-#       distance_list<-c(distance_list,distance(sample[i,x_cols],thesample[j,x_cols]))}
-#     voting_rows<-which(distance_list %in% head(sort(distance_list),k))[1:k]
-#     votes<-sample[voting_rows,y_col]
-#     votes<-factor(votes)
-#     results<-c(results,names(sort(summary(votes),decreasing=TRUE)[1]))
-#     print(results)
-#   }
-#   return(results)
-# }
-# knn.fn(mytrain2,3,sparse_99_col_index,9)
-
+id <- seq(1:979)
+submit <- as.data.frame(cbind(id,sentiment))
+write_csv(submit, 'knnsubmission.csv')
 
