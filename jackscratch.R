401261676cdd325fa24b91a52be50b1f7f246e26
jprominski <jmp3mk@virginia.edu>
2017-09-20 06:37:07 -0400

    knn update

--- jackscratch.R
+++ jackscratch.R
@@ -1,43 +1,31 @@
+library(tidyverse)
 
-sample <- mytrain2
-y_col <- 3
-x_cols <- sparse_99_col_index
-k <- 3
-
-
-distance<-function(myrow,compare_row){
-  return(sqrt(sum((myrow-compare_row)^2)))}
-
-results<-c()
-
-for (i in 1:nrow(sample)){
-  distance_list<-c()
-  thesample<-sample[-i,]
-  for (j in 1:nrow(thesample)){
-    distance_list<-c(distance_list,distance(sample[i,x_cols],thesample[j,x_cols]))
-  }
-  voting_rows<-which(distance_list %in% head(sort(distance_list),k))[1:k]
-  votes<-sample[voting_rows,y_col]
-  votes<-factor(votes)
-  results<-c(results,names(sort(summary(votes),decreasing=TRUE)[1]))
-  print(results)
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
 }
 
-mytrain3 <- mytrain2[,sparse_99_col_index]
-
-
-d_l <- 1:nrow(thesample)
-distance_list <- sapply(d_l,FUN = distance,myrow = sample[i,x_cols],compare_row = thesample[d_l,x_cols])
-# distance_list <- lapply(1:nrow(thesample),FUN = distance, myrow=sample[i,x_cols], compare_row=thesample[x,x_cols])
-
-
+knn_j <- function(x){
+  tr_matrix <- rbind(comb_clean[comb_clean$dataset == 'train',sparse_99_col_index],x)
+  dist_matrix <- as.matrix(dist(tr_matrix))
+  diag(dist_matrix) <- NA
+  nearest <- head(order(dist_matrix[,ncol(dist_matrix)]), n=k) %>% comb_clean[.,]
+  score <- Mode(nearest$sentiment)
+  return(score)
+}
 
 k <- 3
-train <- comb_clean[comb_clean$dataset == 'train',sparse_99_col_index]
-di<-as.matrix(dist(train))
-diag(di) <- NA
+score <- numeric()
+test <- comb_clean[comb_clean$dataset == 'test',sparse_99_col_index]
+score <- apply(test, 1, knn_j)
 
-head(order(di[,1]), n=k)
+
+
+
+# for (i in 1:nrow(test)){
+# score <- c(score,knn_j(test[i,],3))
+# }
+
 
 
 
--- knn(from scratch).R
+++ knn(from scratch).R
@@ -1,54 +1,47 @@
-x1 <- sample(c(0,1), replace=TRUE, size=100)
-x2 <- sample(c(0,1), replace=TRUE, size=100)
-x3 <- sample(c(0,1), replace=TRUE, size=100)
-y<-sample(c(1,2,3,4,5),replace=TRUE,size=100)
-mydata<-as.data.frame(cbind(x1,x2,x3,y))
-sample<-mydata
-k<-5
-y<-4
-x<-c(1:3)
-myrow<-c(1,1,1)
-# ----------------------
+library(tidyverse)
 
-knn.fn<-function(sample,y_col,x_cols,k){
-  #y_col: column number of y in sample
-  #x_cols: list of column numbers of predictors(x) in sample
-  distance<-function(myrow,compare_row){
-    return(sqrt(sum((myrow-compare_row)^2)))}
-  results<-c()
-  for (i in 1:nrow(sample)){
-    distance_list<-c()
-    thesample<-sample[-i,]
-    for (j in 1:nrow(thesample)){
-      distance_list<-c(distance_list,distance(sample[i,x_cols],thesample[j,x_cols]))}
-    voting_rows<-which(distance_list %in% head(sort(distance_list),k))[1:k]
-    votes<-sample[voting_rows,y_col]
-    votes<-factor(votes)
-    results<-c(results,names(sort(summary(votes),decreasing=TRUE)[1]))
-    print(results)
-  }
-  return(results)
-}
-knn.fn(mytrain2,3,sparse_99_col_index,9)
-
-
-
-knn2 <- function(sample, k){
-  n <- nrow(sample)
-  neighbor <- matrix(0, nrow = n, ncol = k)
-  for(i in 1:n) {
-    euc.dist <- colSums((sample[i, ] - t(sample)) ^ 2)  
-    neighbor[i, ] <- order(euc.dist)[2:(k + 1)]
-  }
-  return(neighbor)
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
 }
 
-mytrain3 <- mytrain2[sparse_99_col_index]
+knn_j <- function(x){
+  tr_matrix <- rbind(comb_clean[comb_clean$dataset == 'train',sparse_99_col_index],x)
+  dist_matrix <- as.matrix(dist(tr_matrix))
+  diag(dist_matrix) <- NA
+  nearest <- head(order(dist_matrix[,ncol(dist_matrix)]), n=k) %>% comb_clean[.,]
+  score <- Mode(nearest$sentiment)
+  return(score)
+}
 
-scores <- knn2(mytrain3,3)
+k <- 3
+score <- numeric()
+test <- comb_clean[comb_clean$dataset == 'test',sparse_99_col_index]
+score <- apply(test, 1, knn_j)
 
-scores[333,]
 
-mean(scores)
+
+# JO KNN
+
+# knn.fn<-function(sample,y_col,x_cols,k){
+#   #y_col: column number of y in sample
+#   #x_cols: list of column numbers of predictors(x) in sample
+#   distance<-function(myrow,compare_row){
+#     return(sqrt(sum((myrow-compare_row)^2)))}
+#   results<-c()
+#   for (i in 1:nrow(sample)){
+#     distance_list<-c()
+#     thesample<-sample[-i,]
+#     for (j in 1:nrow(thesample)){
+#       distance_list<-c(distance_list,distance(sample[i,x_cols],thesample[j,x_cols]))}
+#     voting_rows<-which(distance_list %in% head(sort(distance_list),k))[1:k]
+#     votes<-sample[voting_rows,y_col]
+#     votes<-factor(votes)
+#     results<-c(results,names(sort(summary(votes),decreasing=TRUE)[1]))
+#     print(results)
+#   }
+#   return(results)
+# }
+# knn.fn(mytrain2,3,sparse_99_col_index,9)
 
 
